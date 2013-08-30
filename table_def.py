74eb5a8b026bd752639a822de6064e381b85e569
Sam Willcocks <sam@wlcx.cc>
2013-08-30 02:44:28 +0100

    table_def adds tables when run
    Also:
    Tabs->Spaces
    Square brackets derp

--- table_def.py
+++ table_def.py
@@ -1,48 +1,50 @@
 import os
 from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
-engine = create_engine(os.environ['DATABASE_URL', echo=True)
+engine = create_engine(os.environ['DATABASE_URL'], echo=True)
 Base = declarative_base()
 
 class User(Base):
-	__tablename__ = "users"
-	id = Column(Integer, primary_key=True)
-	username = Column(String)
-	firstname = Column(String)
-	lastname = Column(String)
-	collegeid = Column(Integer, ForeignKey("colleges.id"))
-	buildingid = Column(Integer, ForeignKey("buildings.id"))
-	roomnumber = Column(Integer)
-	pwhash = Column(String)
-	biography = Column(String)
-	facebookurl = Column(String)
-	flat = Column(Integer)
-	signup = Column(DateTime)
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    collegeid = Column(Integer, ForeignKey("colleges.id"))
+    buildingid = Column(Integer, ForeignKey("buildings.id"))
+    roomnumber = Column(Integer)
+    pwhash = Column(String)
+    biography = Column(String)
+    facebookurl = Column(String)
+    flat = Column(Integer)
+    signup = Column(DateTime)
 
 class College(Base):
-	__tablename__ = "colleges"
-	id = Column(Integer, primary_key=True)
-	collegename = Column(String)
-	users = relationship("User")
-	buildings = relationship("Building")
+    __tablename__ = "colleges"
+    id = Column(Integer, primary_key=True)
+    collegename = Column(String)
+    users = relationship("User")
+    buildings = relationship("Building")
 
 class Building(Base):
-	__tablename__ = "buildings"
-	id = Column(Integer, primary_key=True)
-	buildingcode = Column(String)
-	buildingname = Column(String)
-	collegeid = Column(Integer, ForeignKey("colleges.id"))
-	buildingtype = Column(String)
-	numflats = Column(Integer)
-	users = relationship("User")
+    __tablename__ = "buildings"
+    id = Column(Integer, primary_key=True)
+    buildingcode = Column(String)
+    buildingname = Column(String)
+    collegeid = Column(Integer, ForeignKey("colleges.id"))
+    buildingtype = Column(String)
+    numflats = Column(Integer)
+    users = relationship("User")
 
 class ValidationLink(Base):
-	__tablename__ = "validationlinks"
-	id = Column(Integer, primary_key=True)
-	key = Column(String)
-	username = Column(String)
-	pwhash = Column(String)
-	created = Column(DateTime)
+    __tablename__ = "validationlinks"
+    id = Column(Integer, primary_key=True)
+    key = Column(String)
+    username = Column(String)
+    pwhash = Column(String)
+    created = Column(DateTime)
+
+Base.metadata.create_all(engine)
